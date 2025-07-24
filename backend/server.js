c681bae5a7bafdc6dbd5b2231208058524eef290
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2025-07-24 12:23:19 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 const authRoutes = require('./routes/authRoutes');
 const mongoDB = require('./config/db');
 mongoDB();
 
 const PORT = 9898;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
